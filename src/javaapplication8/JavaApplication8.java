3da388e22265014594d4432048e98ccc1fa38b33
sena <sena@sena-MS-7817>
2017-04-26 11:02:31 -0500

    parte_3 diego

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -15,7 +15,8 @@ public class JavaApplication8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo como estan");
+        
     }
     
 }
